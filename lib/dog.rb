afbfd81d9c2fbf7cab83a204300a0134b9c69e7e
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-06-01 22:43:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,15 @@
 class Dog
-  def initialize(breed = "Mutt")
+  def initialize(breed = "Mutt", name)
     @breed = breed
+    @name = name
   end
+  def name=(name)
+    @name= name
+  end
+  def name
+    @name
+  end
+
     def breed=(breed)
     @breed = breed
   end
